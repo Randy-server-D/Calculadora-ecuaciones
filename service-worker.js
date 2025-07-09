defe0f1d09e29ac64b573dfebc169affbad922fb
Randy-server-D <estebanrandy060@gmail.com>
2025-07-09 11:51:58 -0600

    Add files via upload
    Calculadora de ecuaciones; agrege los coeficiente en orden si es negativo agrege el negativo

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('solver2x2').then(function(cache) {
+      return cache.addAll([
+        'index.html',
+        'manifest.json',
+        'icon-192.png',
+        'icon-512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
